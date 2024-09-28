45f1016f11ce9821026fe6916d336312df38e87b
Eric <eric3691215@gmail.com>
2024-09-28 12:56:30 -0400

    fixed merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import ChatPage from "./pages/chat-page/chat-page";
-import FlashcardsPage from "./pages/flashcards-page/flashcards-page";
-import AnalyticsPage from "./pages/analytics-page/analytics-page";
-import NavigationBar from "./components/NavigationBar/NavigationBar";
+// import ChatPage from "./pages/chat-page/chat-page";
+// import FlashcardsPage from "./pages/flashcards-page/flashcards-page";
+// import AnalyticsPage from "./pages/analytics-page/analytics-page";
+// import NavigationBar from "./components/NavigationBar/NavigationBar";
 import Auth from './pages/supabase-login/supabase-login';
 import Dashboard from './pages/dashboard-page/dashboard';
 import { useState, useEffect } from 'react';
@@ -35,11 +35,11 @@ function App() {
             path="/dashboard" 
             element={user ? <Dashboard user={user} setUser={setUser} /> : <Navigate to="/" />}
           />
-//         <NavigationBar />
+{/* //         <NavigationBar />
 //         <Routes>
 //           <Route path="/chat-page" element={<ChatPage />} />
 //           <Route path="/flashcards-page" element={<FlashcardsPage />} />
-//           <Route path="/analytics-page" element={<AnalyticsPage />} />
+//           <Route path="/analytics-page" element={<AnalyticsPage />} /> */}
         </Routes>
       </div>
     </Router>
